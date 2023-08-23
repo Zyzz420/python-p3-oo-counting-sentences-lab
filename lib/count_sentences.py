a671c08fa548bf98964d066aea5bdb68bc7874c0
Zyzz king <abdulaziz.komara@student.moringaschool.com>
2023-08-23 13:49:49 -0400

    Assignment complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,22 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            raise TypeError("The value must be a string.")
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        if self.is_sentence() or self.is_question() or self.is_exclamation():
+            sentences = [sentence.strip() for sentence in self._value.split(".") if sentence.strip()]
+            return len(sentences)
+        else:
+            return 0
